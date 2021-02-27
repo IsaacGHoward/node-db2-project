ca2543c903ee3e9368f141c95353edcd024b7459
IsaacGHoward <howardisaac100@gmail.com>
2021-02-26 17:41:28 -0800

    Get cars

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,12 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const db = require('./cars-model');
+const middleware = require('./cars-middleware');
+
+router.get('/', (req, res) => {
+  db.getAll()
+  .then((respon) => {
+    res.send(respon);
+  })
+})
+
+module.exports = router;
